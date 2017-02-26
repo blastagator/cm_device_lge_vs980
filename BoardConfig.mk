459a735d5382523dde480c7d5919dea686487e8d
Greg Wallace <greg@gregtwallace.com>
2017-02-26 09:59:25 -0500

    twrp: custom kernel config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,11 @@
 
 include device/lge/g2-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := lineageos_vs980_defconfig
+ifeq ($(WITH_TWRP),true)
+    TARGET_KERNEL_CONFIG := twrp_vs980_defconfig
+else
+    TARGET_KERNEL_CONFIG := lineageos_vs980_defconfig
+endif
 
 TARGET_OTA_ASSERT_DEVICE := vs980,g2,galbi
 
